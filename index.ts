3b5a7fa9dd84ccead94694824710e185356e5aa0
Vercjames <Vercjames@gmail.com>
2022-08-11 10:39:14 -0500

    service

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response } from "express"
 
-const app = express()
+const server = express()
 
-app.get("/", (req: Request, res: Response) => {
+server.get("/", (req: Request, res: Response) => {
   res.send("Application works!")
 })
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("Application started on port 3000!")
 })
